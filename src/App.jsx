5840488fe61e3902c7982d48c050273c02d0f60a
AMINSNG <aminsng83@gmail.com>
2025-10-08 20:32:37 +0330

    day6

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Training from "./training";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-const handleOnClick = (nmber) => {
-  setCount((count) => count + nmber)
-}
+  const handleOnClick = (nmber) => {
+    setCount((count) => count + nmber);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        {/* <button onClick={() => setCount((count) => count + 1)}> */}
-        {/* <button onClick={handleOnClick}> */}
-        <button onClick={()=>handleOnClick(1)}>
+    <div>
+      <Training />
+    </div>
+    // <>
+    //   <div>
+    //     <a href="https://vite.dev" target="_blank">
+    //       <img src={viteLogo} className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://react.dev" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Vite + React</h1>
+    //   <div className="card">
+    //     {/* <button onClick={() => setCount((count) => count + 1)}> */}
+    //     {/* <button onClick={handleOnClick}> */}
+    //     <button onClick={()=>handleOnClick(1)}>
 
-          count is {count}
-        </button>
-        <button style={{marginLeft:4}}  onClick={() => setCount(0)} >
-          reset
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        salam 
-      </p>
-    </>
-  )
+    //       count is {count}
+    //     </button>
+    //     <button style={{marginLeft:4}}  onClick={() => setCount(0)} >
+    //       reset
+    //     </button>
+    //     <p>
+    //       Edit <code>src/App.jsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     salam
+    //   </p>
+    // </>
+  );
 }
 
-export default App
+export default App;
--- src/training.jsx
+++ src/training.jsx
@@ -1,2 +1,22 @@
-const aray = [amin, ali, morteza, mohammad];
-<button></button>
+import React, { useEffect, useState } from "react";
+import { AiFillApple } from "react-icons/ai";
+
+function Training() {
+  const users = ["amin", "ali", "mohammad"];
+  const [index, Setindex] = useState(0);
+  function createrandom() {
+    const randomnumber = Math.floor(Math.random() * users.length);
+    console.log(randomnumber);
+    Setindex(randomnumber);
+  }
+  return (
+    <div className={users[index] === "ali" ? "activname" : "notactive"}>
+      <div>{users[index]}</div>
+      <button onClick={createrandom}>
+        show random text <AiFillApple/>
+      </button>
+    </div>
+  );
+}
+
+export default Training;
